71c37178e1cdeaa68a46ab90fe35482c453a61e8
fcwg <fcwong@yahoo.com>
2016-01-04 13:39:25 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+
+
+
+
+app.listen(3000, function() {
+	console.log('I am here');
+})
